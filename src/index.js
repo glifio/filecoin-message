b1064729b1c341e76e59305203a6014a0f11f7e3
Thomas O'Brien <tbobrien612@gmail.com>
2020-01-16 14:48:12 -0800

    Build and test for Node and browser (#1)

---
+++ src/index.js
@@ -0,0 +1,117 @@
+const LotusRpcEngine = require('@openworklabs/lotus-jsonrpc-engine')
+const borc = require('borc')
+const base32Function = require('./base32')
+
+const base32 = base32Function('abcdefghijklmnopqrstuvwxyz234567')
+
+let bigintToArray
+let marshalBigInt
+let decodeAddress
+
+class Message {
+  constructor({ To, From, Nonce, Value, Method, GasPrice, GasLimit, Params }) {
+    this.jsonRpcEngine = new LotusRpcEngine.default({
+      apiAddress: 'https://lotus-dev.temporal.cloud/rpc/v0'
+    })
+
+    this.Nonce = Nonce
+
+    // TODO: better validation
+    if (!To) throw new Error('Invalid "to" address')
+    this.To = To
+
+    if (!From) throw new Error('Invalid "from" address')
+    this.From = From
+
+    if (!Value) throw new Error('No value provided')
+    this.Value = Value
+
+    if (typeof Method !== 'number') throw new Error('Invalid "method" passed')
+    this.Method = Method
+
+    if (!GasPrice) throw new Error('No gas price provided')
+    this.GasPrice = GasPrice
+
+    if (!GasLimit) throw new Error('No gas limit provided')
+    this.GasLimit = GasLimit
+
+    this.Params = Params
+  }
+
+  generateNonce = async () => {
+    this.Nonce = await this.jsonRpcEngine.request('MpoolGetNonce', this.From)
+    return true
+  }
+
+  toObj = () => {
+    if (typeof this.nonce !== 'number')
+      throw new Error('Cannot encode message without a nonce')
+    const message = {
+      To: this.To,
+      From: this.From,
+      Nonce: this.Nonce,
+      Value: this.Value,
+      Method: this.Method,
+      GasPrice: '3',
+      GasLimit: '1000',
+      Params: []
+    }
+    return message
+  }
+
+  serialize = () =>
+    new Promise(resolve => {
+      const answer = []
+      answer.push(decodeAddress(this.To))
+      answer.push(decodeAddress(this.From))
+      answer.push(this.Nonce)
+      answer.push(marshalBigInt(this.Value))
+      answer.push(marshalBigInt(this.GasPrice))
+      answer.push(marshalBigInt(this.GasLimit))
+      answer.push(this.Method)
+
+      if (this.Params) {
+        answer.push(this.Params)
+        return resolve(borc.encode(answer))
+      }
+
+      const emptyParamsHeader = new Buffer.alloc(1)
+      emptyParamsHeader[0] = 64
+      const cborWithEmptyParams = Buffer.concat([
+        borc.encode(answer),
+        emptyParamsHeader
+      ])
+      // Change the first byte since cbor is encoded w/o params
+      cborWithEmptyParams[0] = 136
+
+      return resolve(cborWithEmptyParams)
+    })
+}
+
+bigintToArray = v => {
+  let tmp = BigInt(v).toString(16)
+  // not sure why it is not padding and buffer does not like it
+  if (tmp.length % 2 === 1) tmp = `0${tmp}`
+  return Buffer.from(tmp, 'hex')
+}
+
+decodeAddress = address => {
+  const network = address.slice(0, 1)
+  const protocol = address.slice(1, 2)
+  const raw = address.substring(2, address.length)
+  const payloadChecksum = new Buffer.from(base32.decode(raw))
+  const { length } = payloadChecksum
+  const payload = payloadChecksum.slice(0, length - 4)
+  const checksum = payloadChecksum.slice(length - 4, length)
+  const protocolByte = new Buffer.alloc(1)
+  protocolByte[0] = protocol
+  return Buffer.concat([protocolByte, payload])
+}
+
+marshalBigInt = val => {
+  const bigIntOnset = new Buffer.alloc(1)
+  bigIntOnset[0] = 0
+  return Buffer.concat([bigIntOnset, bigintToArray(val)])
+}
+
+module.exports = Message
---
+++ test/message.spec.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+const { expect } = require('chai')
+const Message = require('../dist/index')
+
+const messageObj = {
+  To:
+    't3kl67ybzbqjsu6fr7l4hzuyq5okkwnr2ncabxytl3xmcapupcyzeydbk23bub2dmg2hur4aawpe44w3wptsvq',
+  From:
+    't3f6bifs7c7fuw6mkeycvez3kw3pmwpxbis6agv4563ctdvsqw4gfwq25a3qqiz7womw6xbir5uabgwykazd5a',
+  Nonce: 197,
+  Value: '100000',
+  GasPrice: '1',
+  GasLimit: '1',
+  Method: 0
+}
+
+const messageWithParams = new Message({
+  ...messageObj,
+  Params: Uint8Array.from(
+    Buffer.from('some bytes, idk. probably at least ten of them')
+  )
+})
+const messageWithParamsHexValue =
+  '8858310352fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c64981855ad8681d0d86d1e91e00167939cb5831032f8282cbe2f9696f3144c0aa4ced56dbd967dc2897806af3bed8a63aca16e18b686ba0dc208cfece65bd70a23da0026b18c544000186a042000142000100582e736f6d652062797465732c2069646b2e2070726f6261626c79206174206c656173742074656e206f66207468656d'
+
+const messageWithoutParams = new Message(messageObj)
+const messageWithoutParamsHexValue =
+  '8858310352fdfc072182654f163f5f0f9a621d729566c74d10037c4d7bbb0407d1e2c64981855ad8681d0d86d1e91e00167939cb5831032f8282cbe2f9696f3144c0aa4ced56dbd967dc2897806af3bed8a63aca16e18b686ba0dc208cfece65bd70a23da0026b18c544000186a04200014200010040'
+
+describe('message', async () => {
+  describe('serialize', async () => {
+    it('should serialize message with params', async () => {
+      const serialized = await messageWithParams.serialize()
+      expect(serialized.toString('hex')).to.be.eql(messageWithParamsHexValue)
+    })
+
+    it('should serialize message without params', async () => {
+      const serialized = await messageWithoutParams.serialize()
+      expect(serialized.toString('hex')).to.be.eql(messageWithoutParamsHexValue)
+    })
+  })
+})
