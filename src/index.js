3e5dc18a6a7342d07273309bf0445292fdadad28
listenaddress <tbobrien612@gmail.com>
2020-01-17 11:32:31 -0800

    Validate checksums

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 const LotusRpcEngine = require('@openworklabs/lotus-jsonrpc-engine')
 const borc = require('borc')
-const varint = require('varint')
-const base32Function = require('./base32')
-
-const base32 = base32Function('abcdefghijklmnopqrstuvwxyz234567')
-
-let bigintToArray
-let marshalBigInt
-let decodeAddress
+const { decodeAddress, marshalBigInt } = require('./utils')
 
 class Message {
   constructor({ To, From, Nonce, Value, Method, GasPrice, GasLimit, Params }) {
@@ -87,40 +80,6 @@ class Message {
 
       return resolve(cborWithEmptyParams)
     })
-}
-
-bigintToArray = v => {
-  let tmp = BigInt(v).toString(16)
-  // not sure why it is not padding and buffer does not like it
-  if (tmp.length % 2 === 1) tmp = `0${tmp}`
-  return Buffer.from(tmp, 'hex')
-}
-
-decodeAddress = address => {
-  const network = address.slice(0, 1)
-  const protocol = address.slice(1, 2)
-  const protocolByte = new Buffer.alloc(1)
-  protocolByte[0] = protocol
-  const raw = address.substring(2, address.length)
-
-  if (protocol === '0') {
-    return Buffer.concat([
-      protocolByte,
-      Buffer.from(varint.encode(parseInt(raw)))
-    ])
-  }
-
-  const payloadChecksum = new Buffer.from(base32.decode(raw))
-  const { length } = payloadChecksum
-  const payload = payloadChecksum.slice(0, length - 4)
-  const checksum = payloadChecksum.slice(length - 4, length)
-  return Buffer.concat([protocolByte, payload])
-}
-
-marshalBigInt = val => {
-  const bigIntOnset = new Buffer.alloc(1)
-  bigIntOnset[0] = 0
-  return Buffer.concat([bigIntOnset, bigintToArray(val)])
 }
 
 module.exports = Message
---
+++ src/utils.js
@@ -0,0 +1,58 @@
+const varint = require('varint')
+const { blake2b } = require('blakejs')
+const base32Function = require('./base32')
+
+const base32 = base32Function('abcdefghijklmnopqrstuvwxyz234567')
+
+const bigintToArray = v => {
+  let tmp = BigInt(v).toString(16)
+  // not sure why it is not padding and buffer does not like it
+  if (tmp.length % 2 === 1) tmp = `0${tmp}`
+  return Buffer.from(tmp, 'hex')
+}
+
+const getChecksum = ingest => {
+  return blake2b(ingest, null, 4)
+}
+
+const validateChecksum = (ingest, expect) => {
+  const digest = getChecksum(ingest)
+  return Buffer.compare(Buffer.from(digest), expect)
+}
+
+const decodeAddress = address => {
+  const network = address.slice(0, 1)
+  const protocol = address.slice(1, 2)
+  const protocolByte = new Buffer.alloc(1)
+  protocolByte[0] = protocol
+  const raw = address.substring(2, address.length)
+
+  if (protocol === '0') {
+    return Buffer.concat([
+      protocolByte,
+      Buffer.from(varint.encode(parseInt(raw)))
+    ])
+  }
+
+  const payloadChecksum = new Buffer.from(base32.decode(raw))
+  const { length } = payloadChecksum
+  const payload = payloadChecksum.slice(0, length - 4)
+  const checksum = payloadChecksum.slice(length - 4, length)
+  if (validateChecksum(Buffer.concat([protocolByte, payload]), checksum)) {
+    throw Error("Checksums don't match")
+  }
+
+  return Buffer.concat([protocolByte, payload])
+}
+
+const marshalBigInt = val => {
+  const bigIntOnset = new Buffer.alloc(1)
+  bigIntOnset[0] = 0
+  return Buffer.concat([bigIntOnset, bigintToArray(val)])
+}
+
+module.exports = {
+  bigintToArray,
+  decodeAddress,
+  marshalBigInt
+}
